64f3f6f42ea008bdd07b577da1d44d8067fa76b9
Rafael Pereira <rafaelplaurindo@gmail.com>
2021-01-12 11:39:40 -0300

    adequates to new organization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
-// import { NgCoolFilterPipeModule } from '@rplaurindo/ng-cool-filter-pipe';
-import { NgCoolFilterPipeModule } from 'projects/ng-cool-filter-pipe';
+import { NgCoolFilterPipeModule } from '@actjs.on/ng-cool-filter-pipe';
+// import { NgCoolFilterPipeModule } from 'projects/ng-cool-filter-pipe';
 
 
 @NgModule({
