e27c7346da19d59ae4d8dea14bb111729e62a613
Rafael Pereira <rafaelplaurindo@gmail.com>
2021-02-25 14:40:23 -0300

    adequations

--- projects/ng-cool-filter-pipe/src/lib/filter-by/pipe.ts
+++ projects/ng-cool-filter-pipe/src/lib/filter-by/pipe.ts
@@ -19,13 +19,13 @@ export class FilterByPipe implements PipeTransform {
         this.filter = new Filter();
     }
 
-    transform<T extends Object>(collection: T[] = [], term: string, ...properties: string[]): T[] {
+    transform<T extends Object>(collection: T[], term: string, ...properties: string[]): T[] {
 
         const filtered: T[] = [];
 
         // cause' of asynchronous loading
-        if (!collection.length) {
-            return collection;
+        if (!collection || !collection.length) {
+            return [];
         }
 
         // do not stop to use the getMaps method if the term is empty, otherwise the cache will never be emptied, which may create unexpected results
